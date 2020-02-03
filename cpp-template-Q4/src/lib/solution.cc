30076de66e6f42fb904229541151638420b0a09f
KrishnaSaiTarun <kpasupul@usc.edu>
2020-02-03 13:41:38 -0800

    Question 4 answered

--- cpp-template-Q4/src/lib/solution.cc
+++ cpp-template-Q4/src/lib/solution.cc
@@ -1,6 +1,9 @@
 #include "solution.h"
 
 std::string Solution::PrintHelloWorld() { 
-  return "**** Hello World ****"; 
+
+  std::string name = "Krishna Sai Tarun pasupuleti!";
+  std::string info = "\nThis is for the course EE599- Assignment 1- Q4!";
+  return "My name is " + name + info;
 }
 
